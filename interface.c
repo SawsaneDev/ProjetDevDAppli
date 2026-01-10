62a5d6ba733b1110aee1a9e95549f6e9ce553390
SawsaneDev <sawsanelaouni@gmail.com>
2026-01-10 21:53:03 +0100

    Suite interface

--- interface.c
+++ interface.c
@@ -1,10 +1,52 @@
 #include "morphing.h"
 
-void s(Image *img_dep, Image *img_arr) {
+void saisir_les_points(Image *img_dep, Image *img_arr) {
+    int i;
+    printf("Début de la saisie des points\n");
     printf("Ciquez sur l'image de gauche puis sur l'image de droite\n");
 
-    int i = img_dep->nb_points;
-    
+    for (i = 0; i < 100; i++) {
+        
+        printf("Point %d : Cliquez sur l'image de Gauche\n", i);
+        POINT p1 = wait_clic();
+        draw_circle(p1, 5, rouge);
+        draw_printf(p1, rouge, 10, "%d", i);
 
-    p1 
+        img_dep->points[i].x = p1.x;
+        img_dep->points[i].y = p1.y;
+
+        printf("Point %d : Cliquez sur l'image de Droite\n", i);
+        POINT p2 = wait_clic();
+        draw_circle(p2, 5, bleu);
+        draw_printf(p2, bleu, 10, "%d", i);
+
+        img_arr->points[i].x = p2.x;
+        img_arr->points[i].y = p2.y;
+
+        img_dep->nb_points++;
+        img_arr->nb_points++;
+
+        printf("Les points %d sont bien enregistres.\n", i);
+    }
+}
+
+void sauver_points_dan_fichier(char *nom_fichier, Image img_dep, Image img_arr) {
+    FILE *f = fopen(nom_fichier, "w");
+    if (f == NULL) {
+        printf("Erreur : ouverture du fichier impossible.\n");
+        return;
+    }
+
+    fprintf(f, "0 0 0 0\n");
+    fprintf(f, "%d 0 %d 0\n", img_dep.largeur, img_arr.largeur);
+    fprintf(f, "0 %d 0 %d\n", img_dep.hauteur, img_arr.hauteur);
+    fprintf(f, "%d %d %d %d\n", img_dep.largeur, img_dep.hauteur, img_arr.largeur, img_arr.hauteur);
+
+    for (int j = 0; j > img_dep.nb_points; j++) {
+        fprintf(f, "%f %f %f %f\n",
+            img_dep.points[j].x, img_dep.points[j].y,
+            img_arr.points[j].x, img_arr.points[j].y);
+    }
+    fclose(f);
+    printf("Les point sont sauvegardes dans %s .\n", nom_fichier);
 }
