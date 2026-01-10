e883e0e38c6b0d7732a1894abe64a00075736377
mghofrane <makghofrane@gmail.com>
2026-01-10 18:15:50 +0100

    Create image_interm.c

---
+++ image_interm.c
@@ -0,0 +1,124 @@
+
+typedef struct{
+    unsigned char r,g,b;
+}Pixel;
+
+typedef struct{
+    float x,y;
+}Points;
+
+ typedef struct{
+    int a, b, c;
+}Triangle;
+
+ typedef struct{
+    int largeur, hauteur;
+    Pixel** pixels;
+    Point** points;
+    int nb_points;
+    Triangle* triangles;
+    int nb_triangles;
+}Images;
+
+ int  point_dans_triangle(Point p, Point a, Point b, Point c){
+    float d1= (p.x -b.x)*(a.y -b.y) -(a.x - b.x)*(p.y-b.y);
+    float d2= (p.x -c.x)*(b.y -c.y) -(b.x - c.x)*(p.y-c.y);
+    float d3= (p.x -a.x)*(c.y -a.y) -(c.x - a.x)*(p.y-a.y);
+    return ((d1>=0 && d2>=0 && d3>=0) || (d1<=0 && d2<=0 && d3<=0));
+}
+
+void calcul_points_intermediaire(Point* dep, Point* arr, Point*inter, int n, float alpha){
+    for (int i=0;i<n; i++){
+        inter[i].x= (1-alpha)*dep[i].x + alpha*arr[i].x;
+        inter[i].y= (1-alpha)*dep[i].y + alpha*arr[i].y;
+    }
+}
+
+void triangulation(Point* pts , int n, Triangle* tris, int* nb_tris){
+    tris[0]=(Triangle){0,1,4};
+    tris[1]=(Triangle){1,2,4};
+    tris[2]=(Triangle){2,3,4};
+    tris[3]=(Triangle){3,0,4};
+    *nb_tris=4;
+
+    for (int p=5; p<n; p++){
+        for(int t=0; t< *nb_tris; t++){
+            Triangle tr= tris[t];
+            if (point_dans_triangle(pts[p], pts[tr.a], pts[tr.b], pts[tr.c])){
+                tris[t]= tris[--(*nb_tris)];
+                tris [(*nb_tris)++]= (Triangle){tr.a, tr.b, p};
+                tris [(*nb_tris)++]= (Triangle){tr.b, tr.c, p};
+                tris [(*nb_tris)++]= (Triangle){tr.c, tr.a, p};
+                break;
+            }       
+        }
+
+    }
+}
+
+Pixel interpolation_pixel(Pixel d, Pixel a , float alpha){
+    Pixel p;
+    p.r = (1- alpha)* d.r + alpha* a.r;
+    p.g = (1- alpha)* d.g + alpha* a.g;
+    p.b = (1- alpha)* d.b + alpha* a.b;
+    return p;
+}
+
+void ecrire_ppm(Image img, const char*nom){
+    FILE* f= fopen(nom,"w");
+    if(!f){
+        printf("Impossible d'ouvrir le fichier en ecriture\n");
+        return;
+    }
+
+    fprintf (f, "P3\n");
+    fprintf (f, "%d %d\n", img.largeur, img.hauteur);
+    fprintf (f, "255\n");
+
+    for(int y=0; y<img.hauteur; y++){
+        for(int x=0; x<img.largeur; x++){
+            Pixel p= img.pixels[y][x];
+            fprintf(f, "%d %d %d", p.r, p.g, p.b);
+        }
+        fprintf(f, "\n");
+    }
+
+    fclose (f);
+}
+
+void calcul_image_intermedaire(Image dep, Image arr, Image* inter, int k, int N){
+    float alpha= (float)k/ N;
+    calcul_points_intermediaire(dep.points, arr.points, inter->points, dep.nb_points, alpha);
+    triangulation(inter->points, inter->nb_points, inter->triangles, &inter->nb_triangles);
+    for(int y=0; y<dep.hauteur; y++){
+        for(int x=0; x< dep.largeur; x++){
+            inter->pixels[y][x]= interpolation_pixel(dep.pixels[y][x], arr.pixels[y][x], alpha);
+        }
+    }
+}
+
+void generer_images_intermediaires(Image dep, Image arr, int N){
+    Image inter;
+    inter.largeur= dep.largeur;
+    inter.hauteur= dep.hauteur;
+    inter.nb_points=dep.nb_points;
+    inter.points= malloc(sizeof(Point*) * dep.nb_points);
+    inter.triangles= malloc (sizeof(Triangle*) * (2*dep.nb_points -6));
+
+    inter.pixels= malloc (sizeof(Pixel*) * dep.hauteur);
+    for(int y =0; y<dep.hauteur; y++){
+        inter.pixels[y]= malloc (sizeof(Pixel) * dep.largeur);
+    }
+
+    char filename[64];
+    for (int k=0; k<=N; k++){
+        calcul_image_intermedaire(dep, arr, &inter, k, N);
+        sprintf (filename, "output/image_%03d.ppm", k);
+        ecrire_ppm(inter, filename);
+    }
+
+    for (int y=0; y<dep.hauteur; y++) free(inter.pixels[y]);
+    free(inter.pixels);
+    free(inter.points);
+    free(inter.triangles);
+}
